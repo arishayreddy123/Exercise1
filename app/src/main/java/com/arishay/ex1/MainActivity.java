9b0d33398fd72cd55d8da690d8ceedd14d4e5675
Arishay <arishay.r16@gmail.com>
2025-06-11 20:27:01 +1200

    Exercise 2: Adding Fetch Posts to main menu screen onclick listener.

--- app/src/main/java/com/arishay/ex1/MainActivity.java
+++ app/src/main/java/com/arishay/ex1/MainActivity.java
@@ -3,42 +3,36 @@ package com.arishay.ex1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnAddCar, btnDeleteCar, btnUpdateCar, btnSearchCar;
+    Button btnAddCar, btnDeleteCar, btnUpdateCar, btnSearchCar, btnFetchActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-
         btnAddCar = findViewById(R.id.btnAddCar);
         btnDeleteCar = findViewById(R.id.btnDeleteCar);
         btnUpdateCar = findViewById(R.id.btnUpdateCar);
         btnSearchCar = findViewById(R.id.btnSearchCar);
+        btnFetchActivity = findViewById(R.id.btnFetchActivity);
 
-        btnAddCar.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, AddCarActivity.class);
-            startActivity(intent);
-        });
+        btnAddCar.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, AddCarActivity.class)));
 
-        btnDeleteCar.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, DeleteCarActivity.class);
-            startActivity(intent);
-        });
+        btnDeleteCar.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, DeleteCarActivity.class)));
 
-        btnUpdateCar.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, UpdateCarActivity.class);
-            startActivity(intent);
-        });
+        btnUpdateCar.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, UpdateCarActivity.class)));
 
-        btnSearchCar.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, SearchCarActivity.class);
-            startActivity(intent);
-        });
+        btnSearchCar.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, SearchCarActivity.class)));
+
+        btnFetchActivity.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, FetchPostsActivity.class)));
     }
 }
